cc3e7cadfd74bf9005a5f7243b1b6fa82ff0d685
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-04-03 09:49:07 +0530

    feat: service added

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  website: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  public api: string = 'https://jsonplaceholder.typicode.com/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getUser(): Observable<User[]> {
+    return this.http.get<User[]>(this.api);
+  }
 }
